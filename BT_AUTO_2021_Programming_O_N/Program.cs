2c5c9fdc4ada82bdeda9edcdbaf628da11b12d19
ovidiu nica <ovidiuionut.nica@gmail.com>
2021-12-08 08:39:42 +0200

    ana are mere

--- BT_AUTO_2021_Programming_O_N/Program.cs
+++ BT_AUTO_2021_Programming_O_N/Program.cs
@@ -7,6 +7,8 @@ namespace BT_AUTO_2021_Programming_O_N
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ana are mere si pere");
+            Console.WriteLine("Ioana are struguri");
         }
     }
 }
